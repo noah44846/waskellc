9d56f4feef8e2376e701a6d3548777ef24220f1f
Noah Godel <noah.godel@edu.hefr.ch>
2024-06-11 14:53:19 +0200

    feat: implement type signature parsing

--- waskellc/src/lib.rs
+++ waskellc/src/lib.rs
@@ -1,6 +1,8 @@
 use logos::{Lexer, Logos};
 
-#[derive(Logos, Debug, PartialEq)]
+use std::iter::Peekable;
+
+#[derive(Logos, Debug, Clone, PartialEq)]
 #[logos(skip r"\s+")]
 pub enum Token {
     // any string that starts with a lowercase letter or _ and is followed by any word character (a-z, A-Z, 0-9, _) or '
@@ -34,4 +36,152 @@ pub enum Token {
     
     #[regex(r#""[^"]*""#, |lex| lex.slice()[1..lex.slice().len()-1].to_owned())]
     String(String),
+}
+
+type TokenIter<'a> = Peekable<Lexer<'a, Token>>;
+
+fn next_token(iter: &mut TokenIter, peek: bool) -> Result<Token, String> {
+    let next = if peek {
+        iter.peek().cloned()
+    } else {
+        iter.next()
+    };
+    match next {
+        Some(token) => match token {
+            Ok(token) => Ok(token),
+            Err(_) => Err("Error parsing token".to_string()),
+        },
+        None => Err("Unexpected end of input".to_string()),
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct TopDeclarations(Vec<TopDeclaration>);
+
+impl TopDeclarations {
+    fn parse(input: &mut TokenIter) -> Result<Self, String> {
+        let mut top_declarations = vec![];
+        loop {
+            top_declarations.push(TopDeclaration::parse(input)?);
+            if let None = input.next() {
+                break;
+            }
+        }
+        
+        Ok(TopDeclarations(top_declarations))
+    }
+}
+
+#[derive(Debug)]
+pub enum TopDeclaration {
+    //TypeDecl(TypeDeclaration),
+    //DataDecl(DataDeclaration),
+    //NewTypeDecl(NewTypeDeclaration),
+    // may be a list of type variables
+    TypeSig(String, FunctionType),
+    FunctionDecl(FunctionDeclaration),
+}
+
+impl TopDeclaration {
+    fn parse(input: &mut TokenIter) -> Result<Self, String> {
+        let token = next_token(input, false)?;
+        match token {
+            Token::ReservedIdent(ident) => {
+                match ident.as_str() {
+                    "type" => {
+                        unimplemented!()
+                    },
+                    "data" => {
+                        unimplemented!()
+                    },
+                    "newtype" => {
+                        unimplemented!()
+                    },
+                    _ => Err(format!("Unexpected token: {}", ident)),
+                }
+            },
+            Token::VariableIdent(ident) => {
+                match next_token(input, false)? {
+                    Token::ReservedOperator(op) if op == "::" =>
+                        Ok(TopDeclaration::TypeSig(ident, FunctionType::parse(input)?)),
+                    _ => Ok(TopDeclaration::FunctionDecl(FunctionDeclaration::parse(input)?)),
+                }
+            },
+            _ => Err(format!("Unexpected token: {:?}", token)),
+        }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct FunctionType(Vec<Type>);
+
+impl FunctionType {
+    fn parse(input: &mut TokenIter) -> Result<Self, String> {
+        // example input: a -> b -> c
+        let mut types = vec![];
+        loop {
+            types.push(Type::parse(input)?);
+            match next_token(input, true)? {
+                Token::ReservedOperator(op) if op == "->" => {
+                    input.next(); // consume the operator
+                    continue
+                },
+                _ => break,
+            }
+        }
+        
+        Ok(FunctionType(types))
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug)]
+pub struct Type(Vec<TypeApplicationElement>);
+
+impl Type {
+    fn parse(input: &mut TokenIter) -> Result<Self, String> {
+        match next_token(input, false)? {
+            Token::ConstructorIdent(ident) => {
+                let elements = vec![TypeApplicationElement::TypeConstructor(ident)];
+                Ok(Type(elements))
+            },
+            t => Err(format!("Expected type constructor, got {:?}", t)),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum TypeApplicationElement {
+    //Unit,
+    //ListConstructor,
+    //TupleConstructor(i32),
+    //FunctionConstructor,
+    //ListType(Box<Type>),
+    //TupleType(Vec<Type>),
+    //TypeVariable(String),
+    //ParenthesizedType(Box<Type>),
+    TypeConstructor(String),
+}
+
+#[derive(Debug)]
+pub struct FunctionDeclaration {
+    /*pub name: String,*/
+    /*pub args: Vec<String>,*/
+    /*pub body: Expr,*/
+}
+
+impl FunctionDeclaration {
+    fn parse(_input: &mut TokenIter) -> Result<Self, String> {
+        unimplemented!("FunctionDeclaration::parse")
+    }
+}
+
+pub fn parse(input: String) {
+    let mut tokens = Token::lexer(input.as_str()).peekable();
+
+    let top_declarations = TopDeclarations::parse(&mut tokens);
+
+    println!("{:?}", top_declarations);
 }
--- waskellc/src/main.rs
+++ waskellc/src/main.rs
@@ -1,15 +1,7 @@
-use waskellc::Token;
-
-use logos::Logos;
-
 use std::fs;
 
 fn main() {
     let file_contents = fs::read_to_string("examples/x_square.wsk").unwrap();
 
-    let lex = Token::lexer(&file_contents);
-
-    lex.into_iter().for_each(|token| {
-        println!("{:?}", token);
-    });
+    waskellc::parse(file_contents);
 }
